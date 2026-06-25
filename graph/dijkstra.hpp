4e022703f3dde3e4f7f5bac9b3e715f9ed0f9c80
ZenReKkyo <birds.left.down.up.right@gmail.com>
2026-06-26 05:34:42 +0900

    dijkstra and minor revision

---
+++ graph/dijkstra.hpp
@@ -0,0 +1,37 @@
+#include "procon-library/template/template.hpp"
+template <typename T>
+vector<T> shortest_path(const vector<vector<pair<ll,T>>>& Graph,vector<int>s){
+	int n=Graph.size();
+	vector<T> dist(n,numeric_limits<T>::max());
+	for(auto &e:s) dist[e]=0;
+	priority_queue<pair<T,int>,vector<pair<T,int>>,greater<pair<T,int>>> pq;
+	for(auto &e:s) pq.push({0,e});
+	while(!pq.empty()){
+		auto [d,v]=pq.top(); pq.pop();
+		if(dist[v]<d)continue;
+		for(auto [u,c]:Graph[v]){
+			if(chmin(dist[u],dist[v]+c))pq.push({dist[u],u});
+		}
+	}
+	return dist;
+}
+
+template <typename T>
+vector<T> dense_shortest_path(const vector<vector<pair<ll,T>>>& Graph,vector<int>s){
+    int n=Graph.size();
+    vector<T> dist(n,numeric_limits<T>::max());
+    vector<bool> used(n,false);
+    for(auto &e:s) dist[e]=0;
+    while(true){
+        int v=-1;
+        for(int i=0;i<n;i++){
+            if(!used[i]&&(v==-1||dist[i]<dist[v])) v=i;
+        }
+        if(v==-1) break;
+        used[v]=true;
+        for(auto [u,c]:Graph[v]){
+            chmin(dist[u],dist[v]+c);
+        }
+    }
+    return dist;
+}
---
+++ test/verify/aoj-grl-1-a.cpp
@@ -0,0 +1,19 @@
+// competitive-verifier: PROBLEM http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A
+#include "../../template/template.hpp"
+#include "../../graph/dijkstra.hpp"
+
+int main(){
+	ll V,E,r;
+	cin>>V>>E>>r;
+	vector<vector<pair<ll,ll>>> Graph(V);
+	rep(i,E){
+		ll s,t,d;
+		cin>>s>>t>>d;
+		Graph[s].push_back({t,d});
+	}
+	auto res=shortest_path(Graph,{int(r)});
+	rep(i,V){
+		if(res[i]==numeric_limits<ll>::max())cout<<"INF"<<endl;
+		else cout<<res[i]<<endl;
+	}
+}
